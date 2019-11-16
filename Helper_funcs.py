0cc2e7595ff64d02e92a45c4f091baf7b0b603fb
Giuseppe <gcrosti@gmail.com>
2019-11-16 17:29:07 -0500

    adding alg to app

--- Helper_funcs.py
+++ Helper_funcs.py
@@ -1,5 +1,17 @@
 
 
+from itertools import combinations_with_replacement
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+
+# CREATE CLIENT
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/giuseppecrosti/Documents/1weekoffood/client_secret.json', scope)
+client = gspread.authorize(creds)
+worksheet_data = client.open("Data_1weekMVP")
+
 #GLUTEN-FREE FILTER
 def gf_filter(meals,gf):
     meals1 = meals
@@ -14,4 +26,25 @@ def veg_filter(meals,veg):
         meals1 = meals[meals['Vegetarian?']=='y'] 
     return meals1
 
-#
+#GENERATE MEALS
+def generate_meals(inputs):
+    meals = pd.DataFrame(worksheet_data.get_worksheet(6).get_all_records())
+    meals1 = gf_filter(meals,inputs['s_gf'])
+    meals2 = veg_filter(meals1,inputs['s_veg'])
+    total_portions = int(7 * (int(inputs['s_lunch_p'][:len(inputs['s_lunch_p'])-1])/100) + 7 * (int(inputs['s_dinner_p'][:len(inputs['s_dinner_p'])-1])/100)) * int(inputs['s_mouths'])
+    meal_combos = list(combinations_with_replacement(meals2['id'],total_portions))
+    #meal_combos2 = [x for x in meal_combos if len(set(x)) > 2]
+    #for combo in meal_combos:
+        # if len(set(combo)) < 3:
+        #     continue
+        # set_combo = set(combo)
+        # count = 0
+        # for meal in set_combo:
+        #     if int(combo.count(meal)) % int(meals2[meals2['id']==meal]['max_min_portion']) > 1:
+        #         break
+        #     count += 1
+        #     if count == len(set_combo):
+        #         meal_combos2.append(combo)
+        #         count = 0
+    return meal_combos
+
--- main.py
+++ main.py
@@ -2,20 +2,24 @@
 from pprint import pprint as pp
 from flask import Flask, flash, redirect, render_template, request, url_for
 from ingredients import query_api
+from Helper_funcs import generate_meals
+
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template(
         'inputs.html',
-        data=[{'name':'Toronto'}, {'name':'Montreal'}, {'name':'Calgary'},
-        {'name':'Ottawa'}, {'name':'Edmonton'}, {'name':'Mississauga'},
-        {'name':'Winnipeg'}, {'name':'Vancouver'}, {'name':'Brampton'}, 
-        {'name':'Quebec'}])
+        mouths=[1,2,3,4,5],percentages = ['0%','25%','50%','75%','100%'],yesno = ['yes','no'])
 @app.route("/result" , methods=['GET', 'POST'])
 def result():
     error = None
-    select = request.form.get('comp_select')
-    data = query_api(select)
+    inputs = {}
+    inputs['s_mouths'] = request.form.get('mouths')
+    inputs['s_lunch_p'] = request.form.get('lunch_p')
+    inputs['s_dinner_p'] = request.form.get('dinner_p')
+    inputs['s_veg'] = request.form.get('veg')
+    inputs['s_gf'] = request.form.get('gf')
+    data = generate_meals(inputs)
     return render_template(
         'result.html',
         tables=data,
