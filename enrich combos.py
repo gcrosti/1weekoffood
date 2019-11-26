ff155fae486b5b9af647dcb92d80481e289852c4
Giuseppe <gcrosti@gmail.com>
2019-11-26 09:14:00 -0500

    fixing enriching funcs

--- enrich combos.py
+++ enrich combos.py
@@ -25,21 +25,17 @@ meals = pd.DataFrame(worksheet_data.get_worksheet(6).get_all_records())
 
 #GLUTEN-FREE FILTER
 def gf_filter(combo):
-    ans = 'y'
     for meal in set(combo):
-        if str(meals[meals['id']==meal]['Gluten-free?'])=='n':
-            ans = 'n'
-            break
-    return ans
+        if meals[meals['id']==meal]['Gluten-free?'].values[0]=='n':
+            return 'n'      
+    return 'y'
 
 #VEGETARIAN FILTER
 def veg_filter(combo):
-    ans = 'y'
     for meal in set(combo):
-        if str(meals[meals['id']==meal]['Vegetarian?']) =='n':
-            ans = 'n'
-            break
-    return ans
+        if meals[meals['id']==meal]['Vegetarian?'].values[0] =='n':
+            return 'n'
+    return 'y'
 
 #TIME CALCULATION
 def enter_time(combo):
@@ -64,29 +60,14 @@ for combo in combos['combo']:
     passive_time.append(t['passive'])
     veg.append(veg_filter(combo))
     gf.append(gf_filter(combo))
-#%%
-active_time = []
-for combo in combos['combo']:
-    active_time.append(enter_time(combo)['active'])
 
-active_time
 #%%
 combos['active_time'] = active_time
-#combos['passive_time'] = passive_time
-#combos['veg'] = veg
-#combos['gf'] = gf
+combos['passive_time'] = passive_time
+combos['veg'] = veg
+combos['gf'] = gf
 combos.head()
-
 
 #%%
 combos.to_pickle('combos_enriched.pkl')
-#%%
-combos_enriched_pkl = pd.read_pickle('/Users/giuseppecrosti/Documents/1weekoffood/combos_enriched.pkl')
-combos_enriched_pkl.head()
-
-
-
-#%%
-combos_enriched_pkl[combos_enriched_pkl['gf']=='n'].head()
-
 #%%
