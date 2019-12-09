02440e2422446386f47297942f83cd1f48559acb
Giuseppe <gcrosti@gmail.com>
2019-12-09 14:40:15 -0500

    updates and moving to gcloud

--- output_generator.py
+++ output_generator.py
@@ -11,12 +11,16 @@ import pandas as pd
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/giuseppecrosti/Documents/1weekoffood/client_secret.json', scope)
 client = gspread.authorize(creds)
+worksheet_data = client.open("Data_1weekMVP")
+
 
 # OPEN ENRICHED COMBOS
 enriched_combos = pd.read_pickle('/Users/giuseppecrosti/Documents/1weekoffood/combos_enriched.pkl')
 
 # OPEN OTHER DATASETS
-#meals = pd.DataFrame(worksheet_data.get_worksheet(6).get_all_records())
+meals = pd.DataFrame(worksheet_data.get_worksheet(6).get_all_records())
+meals_recipes = pd.DataFrame(worksheet_data.get_worksheet(4).get_all_records())
+recipes = pd.DataFrame(worksheet_data.get_worksheet(0).get_all_records())
 
 # FORMAT INPUTS
 def format_inputs(x):
@@ -31,6 +35,16 @@ def format_inputs(x):
 
 def output(inputs):
     f_inputs = format_inputs(inputs)
-    return enriched_combos[enriched_combos['veg']=='n'][enriched_combos['gf']==f_inputs['gf']][enriched_combos['portion']==f_inputs['mouths']].head()
+    possible_combos = enriched_combos[enriched_combos['veg']==f_inputs['veg']][enriched_combos['gf']==f_inputs['gf']][enriched_combos['portion']==f_inputs['mouths']]
+    out = pd.DataFrame()
+    sample = possible_combos.sample() 
+    out['meal_id'] = [x for x in set(sample['combo'].values[0])]
+    out['meal_name'] = [meals[meals['id']==x]['Meal_name'].values[0] for x in out['meal_id']]
+    out['recipes'] = [meals_recipes[meals_recipes['Meal_id']==x]['Recipe_name'].values for x in out['meal_id']]
+    out['recipe_urls'] = [meals_recipes[meals_recipes['Meal_id']==x]['recipe_urls'].values for x in out['meal_id']]
+    out['servings'] = [sample['combo'].values[0].count(x) for x in out['meal_id']]
+    return out
+    
+
 
     
