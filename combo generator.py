4775c3b14782e626f69138187489c22ab03b4039
Giuseppe Crosti <giuseppecrosti@Giuseppes-MBP.fios-router.home>
2020-02-21 18:11:55 -0500

    fixed combo generator, refactored enrich combos

--- combo generator.py
+++ combo generator.py
@@ -4,6 +4,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 from itertools import combinations_with_replacement
 import numpy as np 
 from collections import Counter
+import pickle
+import pprint
 
 #%% CREATE CLIENT
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
@@ -21,7 +23,7 @@ recipes = worksheet_data.get_worksheet(0).get_all_records()
 
 
 #%% GENERATE ALL MEAL COMBO ITERATORS
-percentages = [0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3]
+percentages = [0.25,0.5,0.75,1,1.25,1.5,1.75,2]
 all_combos = {}
 for percentage in percentages:
     portion = 7*percentage
@@ -48,7 +50,7 @@ def toomanyleftovers(combo):
 #%% GENERATE COMBOS
 meal_combos = {}
 for portion in all_combos:
-    print(portion)
+    #print(portion)
     meal_combos[portion] = []
     for combo in all_combos[portion]:
         if not combo:
@@ -56,20 +58,90 @@ for portion in all_combos:
         if toomanyleftovers(combo):
             continue
         meal_combos[portion].append(combo)
-        if len(meal_combos[portion]) > 20:
+        if len(meal_combos[portion]) > 50:
             break
+#%%
+print(meal_combos.keys())
+#%% GENERATE REMAINING COMBOS
+empty_portions = []
+existing_portions = meal_combos.keys()
+for mouth in range(2,6):
+    for amt in existing_portions:
+        portion = amt*mouth
+        if portion in existing_portions:
+            continue
+        empty_portions.append(portion)
+
+
+print(len(empty_portions),empty_portions)
+
+#%% PAIR ADDITION OF COMBOS
+new_combos = {}
+for pair in combinations_with_replacement(existing_portions,2):
+    #print(pair)
+    sumpair = sum(pair)
+    if sumpair > 70:
+        continue
+    if sumpair in empty_portions:
+        new_combos[sumpair] = [x+y for x,y in zip(meal_combos[pair[0]],meal_combos[pair[1]])]
+        empty_portions.remove(sumpair)
+
+print(len(empty_portions),new_combos.keys())
+
+#%% TRIP ADDITION OF COMBOS
+for trip in combinations_with_replacement(existing_portions,3):
+    sumtrip = sum(trip)
+    if sumtrip > 70:
+        continue
+    if sumtrip in empty_portions or float(sumtrip) in empty_portions:
+        new_combos[sumtrip] = [x+y+z for x,y,z in zip(meal_combos[trip[0]],meal_combos[trip[1]],meal_combos[trip[2]])]
+        empty_portions.remove(sumtrip)
+
+print(len(empty_portions),new_combos.keys())
+
+#%% PENTA ADDITION OF COMBOS
+for penta in combinations_with_replacement(existing_portions,5):
+    sumpenta = sum(penta)
+    if sumpenta > 70:
+        continue
+    if sumpenta in empty_portions or float(sumpenta) in empty_portions:
+        new_combos[sumpenta] = [x+y+z+a+b for x,y,z,a,b in zip(meal_combos[penta[0]],
+        meal_combos[penta[1]],meal_combos[penta[2]],meal_combos[penta[3]],meal_combos[penta[4]])]
+        empty_portions.remove(sumpenta)
+
+print(len(empty_portions),new_combos.keys())
 
 #%%
-meal_combos
+print(new_combos[70])
+#%% MERGE DICTS
+for portion, combo in new_combos.items():
+    meal_combos[portion] = combo
+
+print(meal_combos.keys())
 #%% PICKLE COMBOS
-#meal_combos_df.to_pickle('all_combos.pkl')
-#%%
+with open('all_combos.pickle','wb') as f:
+    pickle.dump(meal_combos,f,protocol=2)
 
-#%% CREATE ARRAYS
-columns = []
-dts = []
-for k in meals[0].keys():
-    dts.append((k,type(meals[0][k])))
-    columns.append(tuple([meal[k] for meal in meals]))
-#print(dts)
-meals_arr = np.array(columns,dtype=dts)
+
+#%% OLD CODE
+
+for trip in combinations_with_replacement(meal_combos.keys(),3):
+    sumtrip = sum(trip)
+    if sum(trip) > 70:
+        continue
+    if sumtrip in empty_portions:
+        meal_combos[sumtrip].append(meal_combos[trip[0]]+meal_combos[trip[1]]+meal_combos[trip[2]])
+
+empty_portions_count2 = len([k for k in meal_combos.keys() if not meal_combos[k]])
+print(empty_portions_count2)
+#%%
+print(len(meal_combos[70]))
+#%%
+for quart in combinations_with_replacement(meal_combos.keys(),4):
+    if sum(quart) < 22:
+        continue
+    if sum(quart) > 70:
+        continue
+    print(sum(quart))
+
+empty_portions_count2 = len([k for k in meal_combos.keys() if not meal_combos[k]])
--- enrich combos.py
+++ enrich combos.py
@@ -4,40 +4,54 @@
 from itertools import combinations_with_replacement, islice
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
 import numpy as np
+import pickle
+
+#%%OPEN DATA
+with open('meals.pickle','rb') as f:
+    meals = pickle.load(f)
+with open('meals_recipes.pickle','rb') as f:
+    meals_recipes = pickle.load(f)
+with open('recipes.pickle','rb') as f:
+    recipes = pickle.load(f)
+with open('all_combos.pickle','rb') as f:
+    all_combos = pickle.load(f)
 
 #%%
-# CREATE CLIENT
-scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/giuseppecrosti/Documents/1weekoffood/client_secret.json', scope)
-client = gspread.authorize(creds)
-worksheet_data = client.open("Data_1weekMVP")
-
-#%% OPEN PICKLED COMBOS
-combos = pd.read_pickle('/Users/giuseppecrosti/Documents/1weekoffood/all_combos.pkl')
-combos.head()
-
-#%% OPEN TABLES
-meals = pd.DataFrame(worksheet_data.get_worksheet(6).get_all_records())
-
+#print(meals)
 #%% HELPER FUNCS
 
+#REFACTORING DATA
+gf_mealids = []
+veg_mealids = []
+timedict = {}
+for meal in meals:
+    aidee = meal['id']
+    if meal['Gluten-free?'] == 'y':
+        gf_mealids.append(aidee)
+    if meal['Vegetarian?'] == 'y':
+        veg_mealids.append(aidee)
+    timedict[aidee] = [meal['total_active_time'],meal['max_passive_time']]
+
+
 #GLUTEN-FREE FILTER
-def gf_filter(combo):
+def is_gf(combo):
     for meal in set(combo):
-        if meals[meals['id']==meal]['Gluten-free?'].values[0]=='n':
-            return 'n'      
-    return 'y'
+        if meal in gf_mealids:
+            return 'y'      
+    return 'n'
 
 #VEGETARIAN FILTER
-def veg_filter(combo):
+def is_veg(combo):
     for meal in set(combo):
-        if meals[meals['id']==meal]['Vegetarian?'].values[0] =='n':
-            return 'n'
-    return 'y'
+        if meal in veg_mealids:
+            return 'y'
+    return 'n'
 
+print(timedict)
+#%%
 #TIME CALCULATION
+
 def enter_time(combo):
     time = {}
     time['active'] = 0
@@ -48,26 +62,40 @@ def enter_time(combo):
     return time
 
 #%% ENRICH COMBOS
-active_time = []
-passive_time = []
-veg = []
-gf = []
-for combo in combos['combo']:
-    if not combo:
-        continue
-    t = enter_time(combo)
-    active_time.append(t['active'])
-    passive_time.append(t['passive'])
-    veg.append(veg_filter(combo))
-    gf.append(gf_filter(combo))
+enriched_combos = []
+for portion, combos in all_combos.items():
+    for combo in combos:
+        combo_dict = {}
+        combo_dict['portion'] = portion
+        combo_dict['combo'] = combo
+        comboset = set(combo)
+        if all(item in veg_mealids for item in comboset):
+            combo_dict['veg'] = 'y'
+            print(combo_dict)
+        else:
+            combo_dict['veg'] = 'n'
+        if all(item in gf_mealids for item in comboset):
+            combo_dict['gf'] = 'y'
+        else:
+            combo_dict['gf'] = 'n'
+        combo_dict['total_active_time'] = sum(timedict[x][0] for x in comboset)
+        combo_dict['max_passive_time'] = max(timedict[x][1] for x in comboset)
+        enriched_combos.append(combo_dict)
+
+enriched_combos[100]
+#%%
+len(enriched_combos)
 
 #%%
-combos['active_time'] = active_time
-combos['passive_time'] = passive_time
-combos['veg'] = veg
-combos['gf'] = gf
-combos.head()
+with open('enriched_combos_l.pickle','wb') as f:
+    pickle.dump(enriched_combos,f,protocol=2)
 
-#%%
-combos.to_pickle('combos_enriched.pkl')
+#%% TESTING
+vegcombos = [(1,1,1,1),(1,1,1,2),(1,1,2,2),(1,2,3,4)]
+for combo in vegcombos:
+    if all(item in veg_mealids for item in set(combo)):
+        print('yay')
+    else:
+        print('womp')
+
 #%%
